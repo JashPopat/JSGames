ceba8d19944a1ad75939d27fb752afc8e8fd98ef
Jash Popat <z5611818@ad.unsw.edu.au>
2025-11-24 20:20:20 +1100

    dashboard develpment begins

--- Game1/src/header.jsx
+++ Game1/src/header.jsx
@@ -1,6 +1,26 @@
-function header (token) {
+import gameImage from './assets/game.png';
+
+function header () {
     return (
-        <></>
+        <>
+            <div className="w-full header align-center">
+                <img src={gameImage} alt="Hello" className="headerImg"></img>
+                <span className='headerBigText'>
+                    <h1>Home</h1> |
+                    <h1>Blanko</h1> |
+                    <h1>Slido</h1> |
+                    <h1>Tetro</h1>
+                </span>
+                <span className='headerSmallText'>
+                    <h1>H</h1> |
+                    <h1>B</h1> |
+                    <h1>S</h1> |
+                    <h1>T</h1>
+                </span>
+            </div>
+            <div className="headerUnder">
+            </div>
+        </>
     )
 }
 
